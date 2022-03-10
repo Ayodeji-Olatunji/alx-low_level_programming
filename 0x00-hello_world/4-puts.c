1efa049667c62463c0e16019c5c8d330f6057666
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-10 04:23:42 -0800

    Printing output using the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Entry point - main()
+ *
+ * About the code
+ *
+ * prints the string "Programming is like building a multilingual puzzle"
+ *
+ * Returns (0) 
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
