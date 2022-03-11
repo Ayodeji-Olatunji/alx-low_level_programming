7e61658d14e4f35eb56dfe4950a51b088a498afa
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-11 13:28:50 -0800

    Prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: prints all single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf(i"\n");
+		i++;
+	}
+
+	return (0);
+}
+
