a52cc8c06cfcb57c7e1a85d9bd5c38bfab54dcc2
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-11 13:39:31 -0800

    Prints all single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: prints all single digit numbers using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
