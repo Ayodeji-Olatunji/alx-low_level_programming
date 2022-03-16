cd11a1ffccce144f7ce07bbd6c659b4156420918
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-16 14:00:19 -0700

    prints the sum of the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 fibonacci numbers starting with 1 & 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int sum, first, second, count;
+
+	sum = 0;
+	first = 0;
+	second = 1;
+	count = 0;
+
+	while (count < 98)
+	{
+		sum = first + second;
+		printf("%d, ", sum);
+		first = second;
+		second = sum;
+		count++;
+	}
+
+	sum = first + second;
+	printf("%d\n", sum);
+
+	return (0);
+}
