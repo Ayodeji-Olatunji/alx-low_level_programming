abeaca10a4f38da739f10b7c24ac3cca40f808a9
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-17 13:58:05 -0700

    Fizzbuzz challenge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,26 @@ int main(void)
 {
 	int n = 1;
 
-	while (n <= 100)
+	while (n < 101)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("buzz ");
+			if (n == 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
-		else if (n % 3 == 0 && n % 5 == 0)
+		else if (n % 3 == 0)
 		{
-			printf("fizzbuzz ");
+			printf("fizz ");
 		}
 		else
 		{
