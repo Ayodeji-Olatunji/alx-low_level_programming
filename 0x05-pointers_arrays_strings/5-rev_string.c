41bc3f78efadae780eda2b19dc2f00d572ba3462
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-21 14:52:29 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int a;
+	int b;
+	int temp;
+	int len;
+
+	while (s[a] != 0)
+	{
+		a++;
+	}
+
+	len = a;
+	a = 0;
+	b = len - 1;
+
+	while (a < b)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+		a++;
+		b++;
+	}
+}
