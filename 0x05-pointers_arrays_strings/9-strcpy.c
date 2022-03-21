3c13506ed9fa0b934b0b1e403f3a38e1db4c5d2a
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-21 16:07:40 -0700

    Copies the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,28 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0;
 	int i;
 
-	while (src[n] != '\0')
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		n++;
+		dest[i] = *(src + i);
 	}
-	i = 0;
+	dest[i] = '\0';
 
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	i = 1;
-
-	while (i <= n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
 	return (dest);
 }
-
