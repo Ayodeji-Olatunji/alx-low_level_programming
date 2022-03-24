ae5601d178b04a5b03f4ae3c1a492db6acae3a3e
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-24 13:21:00 -0700

    Encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,23 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char arr[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = n[j];
+			}
+			j++;
+		}
+		i++;
 	}
-
-	return (s);
+	return (str);
 }
