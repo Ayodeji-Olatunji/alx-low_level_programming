4df55d93bb4fa8fc1853ab4b76ab0d8d798a2b99
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-03-30 06:17:13 -0700

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main h"
+/**
+ * is_palindrome - checks if a string is
+ * palindrome
+ * @s: the string
+ * Return: 0 if it is palindrome and 1
+ * if otherwise.
+ *
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
