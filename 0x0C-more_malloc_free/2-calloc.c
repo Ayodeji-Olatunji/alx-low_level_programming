b656d54c72601522168a657f5834d2daaff1fa20
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-04-07 13:39:14 -0700

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *m;
 
-	if (nmemmb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(size * nmemb);
 	if (array == NULL)
