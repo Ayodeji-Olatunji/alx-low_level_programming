c9848fe855a093d8dfd753700da4eced1968f4bc
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-04-14 14:12:55 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+
 /**
  * print_char - print character
  * @args: list of arguments
@@ -17,6 +18,7 @@ void print_char(va_list args)
  * @args: list of arguments
  *
  */
+
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
@@ -27,6 +29,7 @@ void print_int(va_list args)
  * @args: list of arguments
  *
  */
+
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
@@ -37,6 +40,7 @@ void print_float(va_list args)
  * @args: list of arguments
  *
  */
+
 void print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
