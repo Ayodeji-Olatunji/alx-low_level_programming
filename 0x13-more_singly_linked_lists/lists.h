5cac551d4b591857256d351ef5a043b9b75ff62c
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-04-26 12:48:15 -0700

    Returns the number of elements in linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,5 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
